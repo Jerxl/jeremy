b7e8882127101bcfcc72c7839d06942d300f9018
Jerxl <124600588+Jerxl@users.noreply.github.com>
2024-11-26 15:44:27 +0800

    Update vite.config.js

--- Personal Portfolio/vite.config.js
+++ Personal Portfolio/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jeremy/",
+  base: '/jeremy/',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Default output folder
+  },
 });
